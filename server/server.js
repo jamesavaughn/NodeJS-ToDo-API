37c0dac0afe615e39357d628c9a2adcf3a29bca2
James Vaughn <home@Jamess-MacBook-Pro.local>
2017-10-12 10:57:14 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 //library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 //local imports
@@ -37,6 +38,24 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/12345324
+//create an id variable on the request object
+app.get(`/todos/:id`, (req, res) => {
+    var id = req.params.id;
+      if (!ObjectID.isValid(id)){
+          return res.status(404).send();
+          console.log('ID not valid');
+        }
+        Todo.findById(id).then((todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo}); // send back todo object
+        }).catch((e) => {
+            res.status(400).send();
+        });
 });
 
 app.listen(3000, () =>{
